d2efa28b0baaa9a6da297fc46e1c0e520950e55c
Katsiaryna Dvarakouskaya <kdvarako@1-B-12.42heilbronn.de>
2024-03-25 12:44:40 +0100

    changed strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: kdvarako <kdvarako@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 12:22:14 by kdvarako          #+#    #+#             */
-/*   Updated: 2024/03/25 12:30:04 by kdvarako         ###   ########.fr       */
+/*   Updated: 2024/03/25 12:43:31 by kdvarako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,19 @@
 int	ft_strncmp(const char *str1, const char *str2, size_t n)
 {
 	unsigned int	i;
+	unsigned char	*s1;
+	unsigned char	*s2;
 
 	if (n == 0)
 		return (0);
 	i = 0;
-	while (i < n && str1[i] == str2[i] && str1[i] != '\0' && str2[i] != '\0')
+	s1 = (unsigned char *) str1;
+	s2 = (unsigned char *) str2;
+	while (i < n && s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
 	{
 		i++;
 	}
 	if (i >= n)
 		i--;
-	return ((unsigned char) str1[i] - (unsigned char) str2[i]);
+	return (s1[i] - s2[i]);
 }
