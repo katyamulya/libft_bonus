574286c986067a896dd87659015ca545b3415203
Katsiaryna Dvarakouskaya <kdvarako@1-B-12.42heilbronn.de>
2024-03-25 12:08:18 +0100

    itoa

---
+++ ft_itoa.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kdvarako <kdvarako@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/18 11:54:25 by kdvarako          #+#    #+#             */
+/*   Updated: 2024/03/25 12:07:31 by kdvarako         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+long	ft_numberscount(long n)
+{
+	long	count;
+
+	count = 0;
+	if (n == 0)
+		return (1);
+	if (n < 0)
+	{
+		n = -n;
+		count++;
+	}
+	while (n != 0)
+	{
+		n = n / 10;
+		count++;
+	}
+	return (count);
+}
+
+long	ft_ten(long num)
+{
+	long	i;
+
+	i = 1;
+	num--;
+	while (num > 0)
+	{
+		i = i * 10;
+		num --;
+	}
+	return (i);
+}
+
+char	*ft_createstr(long l, long count, char *str)
+{
+	int	i;
+
+	i = 0;
+	if (l < 0)
+	{
+		str[i] = '-';
+		l = -l;
+		i++;
+		count--;
+	}
+	while (count > 0)
+	{
+		str[i] = l / ft_ten(count) + '0';
+		l = l - l / ft_ten(count) * ft_ten(count);
+		count--;
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_itoa(int n)
+{
+	long	count;
+	char	*str;
+	long	l;
+
+	l = n;
+	count = ft_numberscount(l);
+	str = (char *)malloc(count + 1);
+	if (!str)
+		return (NULL);
+	str = ft_createstr(l, count, str);
+	return (str);
+}
