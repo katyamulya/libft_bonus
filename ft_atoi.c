acd0e7a38a781c1d03fdfbac8feb8614d999815a
Katsiaryna Dvarakouskaya <kdvarako@2-G-8.42heilbronn.de>
2024-03-27 11:51:35 +0100

    modified atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,40 +6,15 @@
 /*   By: kdvarako <kdvarako@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 11:29:57 by kdvarako          #+#    #+#             */
-/*   Updated: 2024/03/25 11:51:38 by kdvarako         ###   ########.fr       */
+/*   Updated: 2024/03/27 11:50:36 by kdvarako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <limits.h>
 
-int	ft_getnum(const char *str, int sym)
-{
-	int		i;
-	long	num;
-
-	i = 0;
-	num = 0;
-	while (str[i] >= '0' && str[i] <= '9' && str[i] != '\0')
-	{
-		num = num * 10;
-		num = num + str[i] - 48;
-		i++;
-	}
-	if ((num * sym) < INT_MIN || (num * sym) > INT_MAX)
-		num = 0;
-	return (num);
-}
-
-int	ft_findfirst(const char *str)
-{
-	int	i;
-
-	i = 0;
-	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
-		i++;
-	return (i);
-}
+static int	ft_getnum(const char *str, int sym);
+static int	ft_findfirst(const char *str);
 
 int	ft_atoi(const char *str)
 {
@@ -65,4 +40,32 @@ int	ft_atoi(const char *str)
 	ptr = &str[i];
 	num = ft_getnum(ptr, sym);
 	return (num * sym);
+}
+
+static int	ft_getnum(const char *str, int sym)
+{
+	int		i;
+	long	num;
+
+	i = 0;
+	num = 0;
+	while (str[i] >= '0' && str[i] <= '9' && str[i] != '\0')
+	{
+		num = num * 10;
+		num = num + str[i] - 48;
+		i++;
+	}
+	if ((num * sym) < INT_MIN || (num * sym) > INT_MAX)
+		num = 0;
+	return (num);
+}
+
+static int	ft_findfirst(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	return (i);
 }
