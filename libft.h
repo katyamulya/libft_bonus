fb58234b4bc159720ec0e86c09e25ab5f3370cb2
Katsiaryna Dvarakouskaya <kdvarako@2-G-8.42heilbronn.de>
2024-03-27 11:42:42 +0100

    modified libft

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kdvarako <kdvarako@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 12:28:33 by kdvarako          #+#    #+#             */
-/*   Updated: 2024/03/25 11:56:07 by kdvarako         ###   ########.fr       */
+/*   Updated: 2024/03/26 15:20:49 by kdvarako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,14 @@
 # include <limits.h>
 # include <ctype.h>
 # include <string.h>
-# include <strings.h>
 # include <fcntl.h>
 # include <unistd.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
 
 int				ft_isalpha(int ch);
 int				ft_isdigit(int ch);
@@ -56,5 +61,16 @@ void			ft_putchar_fd(char c, int fd);
 void			ft_putstr_fd(char *s, int fd);
 void			ft_putendl_fd(char *s, int fd);
 void			ft_putnbr_fd(int n, int fd);
+t_list			*ft_lstnew(void *content);
+void			ft_lstadd_front(t_list **lst, t_list *new);
+int				ft_lstsize(t_list *lst);
+t_list			*ft_lstlast(t_list *lst);
+void			ft_lstadd_front(t_list **lst, t_list *new);
+void			ft_lstadd_back(t_list **lst, t_list *new);
+void			ft_lstdelone(t_list *lst, void (*del)(void*));
+void			ft_lstclear(t_list **lst, void (*del)(void*));
+void			ft_lstiter(t_list *lst, void (*f)(void *));
+t_list			*ft_lstmap(t_list *lst, void *(*f)(void *), \
+					void (*del)(void *));
 
 #endif
