de65ae248784eeba13bcd97a8b2db736b48c0f4b
Katsiaryna Dvarakouskaya <kdvarako@2-G-8.42heilbronn.de>
2024-03-27 11:31:01 +0100

    modified memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: kdvarako <kdvarako@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 11:58:40 by kdvarako          #+#    #+#             */
-/*   Updated: 2024/03/25 12:10:59 by kdvarako         ###   ########.fr       */
+/*   Updated: 2024/03/26 16:42:23 by kdvarako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char		*p_dest;
 	const unsigned char	*p_src;
 
+	if (dest == NULL && src == NULL && n > 0)
+		return (NULL);
 	i = 0;
 	p_dest = dest;
 	p_src = src;
